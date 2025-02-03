01ce411bbb1d11eb5ae4beab23c98e5c4cc22bbd
Evgeniy Datskij <evgenij.datskij@mail.ru>
2025-02-03 08:55:45 +0500

    use and useSaspence

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,26 +1,31 @@
+import { Suspense, use, useState } from 'react';
+import { fetchUsers } from '../../shared/api';
+
 type User = {
   id: string;
   email: string;
 };
 
+const defaultUsersPromise = fetchUsers();
+
 export function UsersPage() {
+  const [userPromise, setUserPromise] = useState(defaultUsersPromise);
+  const refetchUsers = () => {
+    setUserPromise(fetchUsers());
+  };
   return (
     <main className="container mx-auto p-4 pt-10 flex flex-col gap-4">
       <h1 className="text-3xl font-bold underline">Users</h1>
 
-      <CreateUserForm />
-
-      <UserList
-        users={[
-          { id: '1', email: 'sdfdsf@dsf' },
-          { id: '2', email: '123sf@dsf' },
-        ]}
-      />
+      <CreateUserForm refetchUsers={refetchUsers} />
+      <Suspense fallback={<div>Loading....</div>}>
+        <UserList usersPromise={userPromise} />
+      </Suspense>
     </main>
   );
 }
 
-export function CreateUserForm() {
+export function CreateUserForm({ refetchUsers }: { refetchUsers: () => void }) {
   return (
     <form className="flex gap-2">
       <input type="email" className="border p-2 rounded" />
@@ -33,7 +38,8 @@ export function CreateUserForm() {
   );
 }
 
-export function UserList({ users }: { users: User[] }) {
+export function UserList({ usersPromise }: { usersPromise: Promise<User[]> }) {
+  const users = use(usersPromise);
   return (
     <div className="flex flex-col">
       {users.map((user) => (
