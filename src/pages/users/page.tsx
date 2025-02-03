e228d4e04411b276d7d3e99061b9187749c4537e
Evgeniy Datskij <evgenij.datskij@mail.ru>
2025-02-03 08:20:59 +0500

    finish start

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,60 +1,58 @@
-import { useState } from 'react';
-
 type User = {
   id: string;
   email: string;
 };
 
 export function UsersPage() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [email, setEmail] = useState('');
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setUsers([...users, { id: crypto.randomUUID(), email }]);
-    setEmail('');
-  };
-
-  const handleDelete = (id: string) => {
-    setUsers((lastUsers) => lastUsers.filter((user) => user.id !== id));
-  };
   return (
     <main className="container mx-auto p-4 pt-10 flex flex-col gap-4">
       <h1 className="text-3xl font-bold underline">Users</h1>
-      <section>
-        <form className="flex gap-2" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            className="border p-2 rounded"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <button
-            className="bg-blue-500 hover:border-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit">
-            Add
-          </button>
-        </form>
-      </section>
 
-      <section>
-        <div className="flex flex-col">
-          {users.map((user) => (
-            <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2" key={user.id}>
-              {user.email}
+      <CreateUserForm />
 
-              <button
-                className="bg-red-500 hover:bd-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
-                type="button"
-                onClick={() => {
-                  handleDelete(user.id);
-                }}>
-                Delete
-              </button>
-            </div>
-          ))}
-        </div>
-      </section>
+      <UserList
+        users={[
+          { id: '1', email: 'sdfdsf@dsf' },
+          { id: '2', email: '123sf@dsf' },
+        ]}
+      />
     </main>
+  );
+}
+
+export function CreateUserForm() {
+  return (
+    <form className="flex gap-2">
+      <input type="email" className="border p-2 rounded" />
+      <button
+        className="bg-blue-500 hover:border-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="submit">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export function UserList({ users }: { users: User[] }) {
+  return (
+    <div className="flex flex-col">
+      {users.map((user) => (
+        <UserCart key={user.id} user={user} />
+      ))}
+    </div>
+  );
+}
+
+export function UserCart({ user }: { user: User }) {
+  return (
+    <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2">
+      {user.email}
+
+      <button
+        className="bg-red-500 hover:bd-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
+        type="button">
+        Delete
+      </button>
+    </div>
   );
 }
