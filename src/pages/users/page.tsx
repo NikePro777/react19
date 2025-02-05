bc295b9d11ec99079c566f529cc6a951d0ce265f
Evgeniy Datskij <evgenij.datskij@mail.ru>
2025-02-05 10:25:21 +0500

    add useTransition

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,5 +1,5 @@
-import { Suspense, use, useState } from 'react';
-import { fetchUsers } from '../../shared/api';
+import { Suspense, use, useState, useTransition } from 'react';
+import { createUser, deleteUser, fetchUsers } from '../../shared/api';
 
 type User = {
   id: string;
@@ -19,44 +19,84 @@ export function UsersPage() {
 
       <CreateUserForm refetchUsers={refetchUsers} />
       <Suspense fallback={<div>Loading....</div>}>
-        <UserList usersPromise={userPromise} />
+        <UserList usersPromise={userPromise} refetchUsers={refetchUsers} />
       </Suspense>
     </main>
   );
 }
 
 export function CreateUserForm({ refetchUsers }: { refetchUsers: () => void }) {
+  const [email, setEmail] = useState('');
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    startTransition(async () => {
+      e.preventDefault();
+      await createUser({ email, id: crypto.randomUUID() });
+      startTransition(() => {
+        refetchUsers();
+        setEmail('');
+      });
+    });
+  };
   return (
-    <form className="flex gap-2">
-      <input type="email" className="border p-2 rounded" />
+    <form className="flex gap-2" onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="border p-2 rounded"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={isPending}
+      />
       <button
-        className="bg-blue-500 hover:border-blue-700 text-white font-bold py-2 px-4 rounded"
-        type="submit">
+        className="bg-blue-500 hover:border-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-400 cursor-pointer"
+        type="submit"
+        disabled={isPending}>
         Add
       </button>
     </form>
   );
 }
 
-export function UserList({ usersPromise }: { usersPromise: Promise<User[]> }) {
+export function UserList({
+  usersPromise,
+  refetchUsers,
+}: {
+  usersPromise: Promise<User[]>;
+  refetchUsers: () => void;
+}) {
   const users = use(usersPromise);
   return (
     <div className="flex flex-col">
       {users.map((user) => (
-        <UserCart key={user.id} user={user} />
+        <UserCart key={user.id} user={user} refetchUsers={refetchUsers} />
       ))}
     </div>
   );
 }
 
-export function UserCart({ user }: { user: User }) {
+export function UserCart({ user, refetchUsers }: { user: User; refetchUsers: () => void }) {
+  const [isPending, startTransition] = useTransition();
+
+  const handleDelete = async () => {
+    startTransition(async () => {
+      deleteUser(user.id);
+      startTransition(() => {
+        refetchUsers();
+      });
+    });
+  };
+
   return (
     <div className="border p-2 m-2 rounded bg-gray-100 flex gap-2">
       {user.email}
 
       <button
-        className="bg-red-500 hover:bd-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
-        type="button">
+        className="bg-red-500 hover:bd-red-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400 cursor-pointer"
+        onClick={handleDelete}
+        type="button"
+        disabled={isPending}>
         Delete
       </button>
     </div>
