36d60ac7a85acb740a24ade489a48d95fae15d2f
Evgeniy Datskij <evgenij.datskij@mail.ru>
2025-02-18 09:29:50 +0500

    add zustand (don't work transition

--- src/app/entities/user.tsx
+++ src/app/entities/user.tsx
@@ -1,26 +1,13 @@
-import { createContext, startTransition, use, useState } from 'react';
+import { startTransition } from 'react';
 import { fetchUsers, User } from '../../shared/api';
+import { create } from 'zustand';
 
-type UserContextType = {
+type UserState = {
   usersPromise: Promise<User[]>;
   refetchUsers: () => void;
 };
 
-const UsersContext = createContext<UserContextType | null>(null);
-
-const defaultUsersPromise = fetchUsers();
-
-export function UsersProvider({ children }: { children: React.ReactNode }) {
-  const [usersPromise, setUsersPromise] = useState(defaultUsersPromise);
-  const refetchUsers = () => startTransition(() => setUsersPromise(fetchUsers()));
-
-  return <UsersContext value={{ usersPromise, refetchUsers }}>{children}</UsersContext>;
-}
-
-export function useUsersGlobal() {
-  const context = use(UsersContext);
-  if (!context) {
-    throw new Error('useUsers must be used within a UsersProvider');
-  }
-  return context;
-}
+export const useUsersGlobal = create<UserState>()((set) => ({
+  usersPromise: fetchUsers(),
+  refetchUsers: () => startTransition(() => set({ usersPromise: fetchUsers() })),
+}));
